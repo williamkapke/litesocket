f85a3a0ffbc06f06ec2fe7138a7a4f708e0eec76
William Wicks <wjwicks@gmail.com>
2013-09-13 23:09:57 +0000

    rename exports

--- litesocket.js
+++ litesocket.js
@@ -1,20 +1,20 @@
 var debug = require("debug")("litesocket");
 
-exports = module.exports = function(req, res, next){
+litesocket = module.exports = function(req, res, next){
 	debug("connection opened");
-	res.writeHead(200, exports.headers(req));
+	res.writeHead(200, litesocket.headers(req));
 	["sendRetry","sendComment","sendData"].forEach(function(name){
-		var fn = exports[name];
+		var fn = litesocket[name];
 		res[name] = function(val){
 			fn(res, val);
 		};
 	});
 	res.send = function(data, options){
-		exports.send(res, data, options);
+		litesocket.send(res, data, options);
 	};
 
 	//send something or the browser gets mad!
-	res.sendComment(exports.welcome);
+	res.sendComment(litesocket.welcome);
 
 	//very important to remove it from the pool and kill off the default 2 minute timeout
 	req.socket.setTimeout(0);
@@ -25,9 +25,12 @@ exports = module.exports = function(req, res, next){
 
 	next();
 };
+litesocket.handler = function(url, callback){
+	this.get(url, litesocket, callback);
+};
 
-exports.welcome = "You are connected";
-exports.headers = function(res){
+litesocket.welcome = "You are connected";
+litesocket.headers = function(res){
 	return {
 		"Access-Control-Allow-Origin": "*",
 		"Content-Type":"text/event-stream",
@@ -35,31 +38,31 @@ exports.headers = function(res){
 		"Connection":"keep-alive"
 	};
 }
-exports.sendId = function(stream, id){
+litesocket.sendId = function(stream, id){
 	debug("id: ", id);
 	stream.write("id:");
 	stream.write(String(id));
 	stream.write('\n');
 };
-exports.sendEvent = function(stream, name){
+litesocket.sendEvent = function(stream, name){
 	debug("event:", name);
 	stream.write("event:");
 	stream.write(String(name));
 	stream.write('\n');
 };
-exports.sendRetry = function(stream, ms){
+litesocket.sendRetry = function(stream, ms){
 	debug("retry:", ms);
 	stream.write("retry:");
 	stream.write(String(ms));
 	stream.write('\n');
 };
-exports.sendComment = function(stream, comment){
+litesocket.sendComment = function(stream, comment){
 	debug(":", comment);
 	stream.write(":");
 	stream.write(String(comment).replace(/\n(?=.)/g, '\n:'));
 	stream.write('\n');
 };
-exports.sendData = function(stream, data){
+litesocket.sendData = function(stream, data){
 	data = String(data).replace(/\n(?=.)/g, '\ndata: ');
 	stream.write("data:");
 	debug(data);
@@ -68,24 +71,21 @@ exports.sendData = function(stream, data){
 		return;
 	stream.write(/\n\r?$/.test(data)? '\n':'\n\n');
 };
-exports.send = function(stream, data, options){
+litesocket.send = function(stream, data, options){
 	if(options){
 		if(options.comment){
-			exports.sendComment(stream, options.comment);
+			litesocket.sendComment(stream, options.comment);
 		}
 		if(options.retry){
-			exports.sendRetry(stream, options.retry);
+			litesocket.sendRetry(stream, options.retry);
 		}
 		if(options.id){
-			exports.sendId(stream, options.id);
+			litesocket.sendId(stream, options.id);
 		}
 		if(options.event){
-			exports.sendEvent(stream, options.event);
+			litesocket.sendEvent(stream, options.event);
 		}
 	}
 	if(data)
-		exports.sendData(stream, data);
-};
-exports.handler = function sseHandler(url, callback){
-	this.get(url, exports, callback);
+		litesocket.sendData(stream, data);
 };
